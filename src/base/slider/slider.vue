54de6950943becdadb70ae3762b2818bfaf34507
Cunjun Wang <13621691063@163.com>
2019-05-09 23:47:31 +0800

    轮播组建完成

--- src/base/slider/slider.vue
+++ src/base/slider/slider.vue
@@ -3,6 +3,10 @@
     <div class="slider-group" ref="sliderGroup">
       <slot></slot>
     </div>
+    <div class="dots">
+      <span class="dot" :class="{active: currentPageIndex === index}"
+            v-for="(item, index) in dots" :key="index"></span>
+    </div>
   </div>
 </template>
 
@@ -11,6 +15,7 @@
   import {addClass} from '../../common/js/dom'
 
   export default {
+    name: 'slider',
     // 设置组件属性, 外部可以控制
     props: {
       loop: {
@@ -26,16 +31,70 @@
         default: 4000
       }
     },
+    data () {
+      return {
+        dots: [],
+        currentPageIndex: 0
+      }
+    },
     mounted () {
       // 保证dom完成渲染, 加个延时
       // 浏览器通常是17ms刷新一次
       setTimeout(() => {
-        this._setSliderWidth()
+        this._setSliderWidth(false)
+        this._initDots()
         this._initSlider()
+
+        if (this.autoPlay) {
+          this._play()
+        }
       }, 20)
+      // 监听窗口大小改变事件
+      // 若改变了, 重新计算轮播图尺寸,
+      // 防止出现改变窗口尺寸后图片轮播尺寸错误
+      window.addEventListener('resize', () => {
+        // slider初始化之前什么都不做
+        if (!this.slider || !this.slider.enabled) {
+          return
+        }
+        clearTimeout(this.resizeTimer)
+        this.resizeTimer = setTimeout(() => {
+          if (this.slider.isInTransition) {
+            this._onScrollEnd()
+          } else {
+            if (this.autoPlay) {
+              this._play()
+            }
+          }
+          this.refresh()
+        }, 60)
+      })
+    },
+    activated () {
+      this.slider.enable()
+      let pageIndex = this.slider.getCurrentPage().pageX
+      this.slider.goToPage(pageIndex, 0, 0)
+      this.currentPageIndex = pageIndex
+      if (this.autoPlay) {
+        this._play()
+      }
+    },
+    deactivated () {
+      this.slider.disable()
+      clearTimeout(this.timer)
+    },
+    beforeDestroy () {
+      this.slider.disable()
+      clearTimeout(this.timer)
     },
     methods: {
-      _setSliderWidth () {
+      refresh () {
+        if (this.slider) {
+          this._setSliderWidth(true)
+          this.slider.refresh()
+        }
+      },
+      _setSliderWidth (isResize) {
         this.children = this.$refs.sliderGroup.children
         let width = 0
         let sliderWidth = this.$refs.slider.clientWidth
@@ -46,7 +105,7 @@
           width += sliderWidth
         }
 
-        if (this.loop) {
+        if (this.loop && !isResize) {
           width += 2 * sliderWidth
         }
         this.$refs.sliderGroup.style.width = width + 'px'
@@ -58,13 +117,42 @@
             scrollX: true,
             scrollY: false,
             momentum: false,
-            snap: true,
-            snapLoop: this.loop,
-            snapThreshold: 0.3,
-            snapSpeed: 400,
-            click: true
+            snap: {
+              loop: this.loop,
+              threshold: 0.3,
+              speed: 400
+            }
           }
         )
+        this.slider.on('scrollEnd', this._onScrollEnd)
+        this.slider.on('touchend', () => {
+          if (this.autoPlay) {
+            this._play()
+          }
+        })
+        this.slider.on('beforeScrollStart', () => {
+          if (this.autoPlay) {
+            clearTimeout(this.timer)
+          }
+        })
+      },
+      _onScrollEnd () {
+        let pageIndex = this.slider.getCurrentPage().pageX
+        this.currentPageIndex = pageIndex
+        // 每次自动轮播前要clear一次
+        // 场景: 用户手动拖动了一下轮播, 由于timer机制会跳到上一张图再轮播回来
+        if (this.autoPlay) {
+          this._play()
+        }
+      },
+      _initDots () {
+        this.dots = new Array(this.children.length)
+      },
+      _play () {
+        clearTimeout(this.timer)
+        this.timer = setTimeout(() => {
+          this.slider.next()
+        }, this.interval)
       }
     }
   }
