fec7f0c7231e1e0e27196e7d9f8200b012bf4ab2
Cunjun Wang <13621691063@163.com>
2019-05-09 21:43:54 +0800

    通过jsonp和promise获取轮播图数据

---
+++ src/api/url.js
@@ -0,0 +1,3 @@
+// Created by CunjunWang on 2019-05-09
+
+export const homeCarouselUrl = 'https://c.y.qq.com/musichall/fcgi-bin/fcg_yqqhomepagerecommend.fcg'
---
+++ src/common/js/jsonp.js
@@ -0,0 +1,28 @@
+// Created by CunjunWang on 2019-05-08
+
+import originJSONP from 'jsonp'
+
+// 拼接url并调用接口
+export default function jsonp (url, data, option) {
+  url += (url.indexOf('?') < 0 ? '?' : '&') + param(data)
+  return new Promise((resolve, reject) => {
+    console.log('url: ' + url)
+    originJSONP(url, option, (err, data) => {
+      if (!err) {
+        resolve(data)
+      } else {
+        reject(err)
+      }
+    })
+  })
+}
+
+// 拼接data中的参数到url
+function param (data) {
+  let url = ''
+  for (var k in data) {
+    let value = data[k] !== undefined ? data[k] : ''
+    url += `&${k}=${encodeURIComponent(value)}`
+  }
+  return url ? url.substr(1) : ''
+}
