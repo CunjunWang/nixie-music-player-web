2a6ac934807f1b6fca58d0feff9c982ce187a699
Cunjun Wang <13621691063@163.com>
2019-05-11 22:48:53 +0800

    歌手数据处理, 封装Singer类

---
+++ src/api/singer.js
@@ -0,0 +1,18 @@
+// Created by CunjunWang on 2019-05-11
+import jsonp from '../common/js/jsonp'
+import {commonParameter, options} from './config'
+
+export function getSingerList() {
+  const url = 'https://c.y.qq.com/v8/fcg-bin/v8.fcg'
+  const data = Object.assign({}, commonParameter, {
+    channel: 'singer',
+    page: 'list',
+    key: 'all_all_all',
+    pagesize: 100,
+    pagenum: 1,
+    hostUin: 0,
+    needNewCode: 0,
+    platform: 'yqq'
+  })
+  return jsonp(url, data, options)
+}
