1ad8878d34652d3a5f97f1dc0c8e42353de87e5e
Cunjun Wang <13621691063@163.com>
2019-05-30 21:33:18 +0800

    添加播放器内核所需的vuex数据结构

--- src/store/state.js
+++ src/store/state.js
@@ -1,7 +1,21 @@
 // Created by CunjunWang on 2019-05-25
 
+import {playMode} from '../common/js/config'
+
 const state = {
-  singer: {}
+  singer: {},
+  // 是否正在播放
+  playing: false,
+  // 是否全屏
+  fullScreen: false,
+  // 播放列表
+  playList: [],
+  // 歌曲列表
+  sequenceList: [],
+  // 播放模式
+  mode: playMode.sequence,
+  // 当前播放歌曲index
+  currentIndex: -1
 }
 
 export default state
