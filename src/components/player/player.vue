ec31b1df5bad86c1a9a2ef5a39b6633ad0677086
Cunjun Wang <13621691063@163.com>
2019-06-03 22:54:24 +0800

    获取歌曲当前播放时间并对展示做样式; 获取歌曲总时间

--- src/components/player/player.vue
+++ src/components/player/player.vue
@@ -26,6 +26,11 @@
           </div>
         </div>
         <div class="bottom">
+          <div class="progress-wrapper">
+            <span class="time time-l">{{format(currentTime)}}</span>
+            <div class="progress-bar-wrapper"></div>
+            <span class="time time-r">{{format(currentSong.duration)}}</span>
+          </div>
           <div class="operators">
             <div class="icon i-left">
               <i class="icon-sequence"></i>
@@ -68,7 +73,8 @@
         </div>
       </div>
     </transition>
-    <audio ref="audio" :src="currentSong.url" @canplay="ready" @error="error"></audio>
+    <audio ref="audio" :src="currentSong.url" @canplay="ready" @error="error"
+           @timeupdate="updateTime"></audio>
   </div>
 </template>
 
@@ -82,7 +88,8 @@
   export default {
     data () {
       return {
-        songReady: false
+        songReady: false,
+        currentTime: 0
       }
     },
     computed: {
@@ -192,6 +199,24 @@
       },
       error () {
         this.songReady = true
+      },
+      updateTime (e) {
+        this.currentTime = e.target.currentTime
+      },
+      format (interval) {
+        // 正数向下取整
+        interval = interval | 0
+        const minute = (interval / 60) | 0
+        const second = this._pad(interval % 60)
+        return `${minute}:${second}`
+      },
+      _pad (num, n = 2) {
+        let len = num.toString().length
+        while (len < n) {
+          num = '0' + num
+          len++
+        }
+        return num
       },
       _getPosAndScale () {
         const targetWidth = 40
