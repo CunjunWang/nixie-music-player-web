43505219a77362b7bf9d31eebc9124e9059f0903
Cunjun Wang <13621691063@163.com>
2019-06-10 23:49:11 +0800

    复用music-list组件, 实现推荐榜单歌单列表展示

--- src/components/disc/disc.vue
+++ src/components/disc/disc.vue
@@ -1,12 +1,16 @@
 <template>
   <transition name="slide">
-    <music-list :title="title" :bg-image="bgImage"></music-list>
+    <music-list :title="title" :bg-image="bgImage"
+                :songs="songs"></music-list>
   </transition>
 </template>
 
 <script type="text/ecmascript-6">
   import MusicList from '../../components/music-list/music-list'
   import {mapGetters} from 'vuex'
+  import {getSongList} from '../../api/recommend'
+  import {ERR_OK} from '../../api/config'
+  import {createSong} from '../../common/js/song'
 
   export default {
     computed: {
@@ -19,6 +23,35 @@
       ...mapGetters([
         'disc'
       ])
+    },
+    data () {
+      return {
+        songs: []
+      }
+    },
+    created () {
+      this._getSongList()
+    },
+    methods: {
+      _getSongList () {
+        if (!this.disc.dissid) {
+          this.$router.push('/recommend')
+        }
+        getSongList(this.disc.dissid).then((res) => {
+          if (res.code === ERR_OK) {
+            this.songs = this._normalizeSongs(res.cdlist[0].songlist)
+          }
+        })
+      },
+      _normalizeSongs (list) {
+        let ret = []
+        list.forEach((musicData) => {
+          if (musicData.songid && musicData.albumid) {
+            ret.push(createSong(musicData))
+          }
+        })
+        return ret
+      }
     },
     components: {
       MusicList
