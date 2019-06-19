2e4fd8c25492092e3a3b035d4e139137e850af3a
Cunjun Wang <13621691063@163.com>
2019-06-20 00:10:27 +0800

    修复npm error

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const express = require('express')
 const path = require('path')
 const serveStatic = require('serve-static')
 const axios = require('axios')
+const bodyParser = require('body-parser')
 
 const app = express()
 app.use(serveStatic(path.join(__dirname, 'dist')))
