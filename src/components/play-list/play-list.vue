1d36caf0b543cd145d46026db2dfdc14a65fa4b9
Cunjun Wang <13621691063@163.com>
2019-06-20 22:20:55 +0800

    播放列表操作, 删除、清空操作实现

--- src/components/play-list/play-list.vue
+++ src/components/play-list/play-list.vue
@@ -7,25 +7,26 @@
           <h1 class="title">
             <i class="icon"></i>
             <span class="text"></span>
-            <span class="clear">
+            <span class="clear" @click="showConfirm">
               <i class="icon-clear"></i>
             </span>
           </h1>
         </div>
-        <div class="list-content">
-          <ul>
-            <li class="item">
-              <i class="current"></i>
-              <span class="text"></span>
+        <scroll class="list-content" :data="sequenceList" ref="listContent">
+          <transition-group name="list" tag="ul">
+            <li class="item" v-for="(item, index) in sequenceList"
+                @click="selectItem(item, index)" :key="item.id" ref="listItem">
+              <i class="current" :class="getCurrentIcon(item)"></i>
+              <span class="text">{{item.name}}</span>
               <span class="like">
                 <i class="icon-not-favorite"></i>
               </span>
-              <span class="delete">
+              <span class="delete" @click.stop="deleteOne(item)">
                 <i class="icon-delete"></i>
               </span>
             </li>
-          </ul>
-        </div>
+          </transition-group>
+        </scroll>
         <div class="list-operate">
           <div class="add">
             <i class="icon-add"></i>
@@ -36,24 +37,98 @@
           <span>关闭</span>
         </div>
       </div>
+      <confirm ref="confirm" text="是否清空播放列表" confirm-btn-text="清空"
+               @confirm="confirmClear"></confirm>
     </div>
   </transition>
 </template>
 
 <script type="text/ecmascript-6">
+  import Scroll from '../../base/scroll/scroll'
+  import Confirm from '../../base/confirm/confirm'
+  import {mapGetters, mapMutations, mapActions} from 'vuex'
+  import {playMode} from '../../common/js/config'
+
   export default {
     data () {
       return {
         showFlag: false
       }
     },
+    computed: {
+      ...mapGetters([
+        'sequenceList',
+        'currentSong',
+        'playList',
+        'mode'
+      ])
+    },
     methods: {
       show () {
         this.showFlag = true
+        setTimeout(() => {
+          this.$refs.listContent.refresh()
+        }, 20)
+        this.scrollToCurrent(this.currentSong)
       },
       hide () {
         this.showFlag = false
+      },
+      getCurrentIcon (item) {
+        if (this.currentSong.id === item.id) {
+          return 'icon-play'
+        }
+        return ''
+      },
+      selectItem (item, index) {
+        if (this.mode === playMode.random) {
+          index = this.playList.findIndex((song) => {
+            return song.id === item.id
+          })
+        }
+        this.setCurrentIndex(index)
+        this.setPlayingState(true)
+      },
+      scrollToCurrent (current) {
+        const index = this.sequenceList.findIndex((song) => {
+          return current.id === song.id
+        })
+        this.$refs.listContent.scrollToElement(
+          this.$refs.listItem[index], 300)
+      },
+      deleteOne (item) {
+        this.deleteSong(item)
+        if (!this.playList.length) {
+          this.hide()
+        }
+      },
+      showConfirm () {
+        this.$refs.confirm.show()
+      },
+      confirmClear () {
+        this.deleteSongList()
+        this.hide()
+      },
+      ...mapMutations({
+        setCurrentIndex: 'SET_CURRENT_INDEX',
+        setPlayingState: 'SET_PLAYING_STATE'
+      }),
+      ...mapActions([
+        'deleteSong',
+        'deleteSongList'
+      ])
+    },
+    watch: {
+      currentSong (newSong, oldSong) {
+        if (!this.showFlag || (newSong.id === oldSong.id)) {
+          return
+        }
+        this.scrollToCurrent(newSong)
       }
+    },
+    components: {
+      Scroll,
+      Confirm
     }
   }
 </script>
